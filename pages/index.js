82001416820daa6c314cef789190c8db1748f228
TemurMalik <tmaq77@gmail.com>
2026-03-05 14:29:35 +0500

    Add About And Contact

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-
+import style from "../styles/Home.module.css";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>Welcome to Next.js!</h1>
+      <Head>
+        <title>Home</title>
+      </Head>
     </>
   );
 }
